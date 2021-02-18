566a8385a3c68db7709d9741e4dd5950151a7bbd
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-02-18 15:28:10 +0100

    Added show-all-cocktails user story

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,10 @@ end
 10.times do
   Ingredient.create(name: pick_an_ingredient(data))
 end
+
+Cocktail.create(name: 'Vodka martini', image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/vodka-martini-recipe-d7e9995.jpg?quality=90&webp=true&resize=440,400')
+Cocktail.create(name: 'Espresso martini', image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/espresso-martini-f099531.jpg?quality=90&webp=true&resize=440,400')
+Cocktail.create(name: 'Pink negroni', image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/classic-negroni-dfc0ca9.jpg?quality=90&webp=true&resize=440,400')
+Cocktail.create(name: 'Sweet manhattan cocktail', image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/manhattan-cocktail-4d775c9.jpg?quality=90&webp=true&resize=440,400')
+Cocktail.create(name: 'Mango & pineapple mojito', image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mango-and-pineapple-mojito-15b1c1a.jpg?quality=90&webp=true&resize=440,400')
+Cocktail.create(name: 'Frozen margarita', image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/frozen-margarita-e3ddfe9.jpg?quality=90&webp=true&resize=440,400')
