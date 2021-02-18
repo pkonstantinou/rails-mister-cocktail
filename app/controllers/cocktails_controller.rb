4fef6c42bc57a4453fdf7dad57642391b77eaa4e
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-02-18 17:44:32 +0100

    Added add-new-cocktail user story

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,9 +3,26 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
-  def show; end
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @cocktail = Cocktail.new
+  end
 
-  def create; end
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params[:cocktail].permit(:name, :image_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: :index
+  resources :cocktails, only: %i[index show new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
